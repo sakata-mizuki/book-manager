ee87c4d1a838b06ee9de35da74fb8db265f652cb
NobutoMyodo <M.Nobuto3527@gmail.com>
2025-05-07 00:12:55 +0900

    提出ファイル
    確認お願いします。

---
+++ bookmanager/controller/ApiController.java
@@ -0,0 +1,45 @@
+package com.yi_college.bookmanager.controller;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yi_college.bookmanager.model.Book;
+import com.yi_college.bookmanager.service.BookService;
+
+@RestController
+@RequestMapping("/api/books")
+public class ApiController {
+
+    private final BookService bookService;
+
+    public ApiController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public List<Book> getBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    public Book createBook(@RequestBody Book book) {
+        return bookService.createBook(book);
+    }
+
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable int id, @RequestBody Book book) {
+        return bookService.updateBook(id, book);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable int id) {
+        bookService.deleteBook(id);
+    }
+}
